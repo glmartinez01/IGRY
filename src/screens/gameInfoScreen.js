72e0b664cd8d66a043a9b8564ce0660044dc6746
Kerros13 <73565783+Kerros13@users.noreply.github.com>
2020-11-10 23:03:43 -0600

    24_commit
    Recontruimos completamente la pantalla de la informacion de los juegos y agregamos dos scrolls 1 horizontal y otro vertical, y hicimos un arreglo de imagenes para background de la informacion

--- src/screens/gameInfoScreen.js
+++ src/screens/gameInfoScreen.js
@@ -5,7 +5,6 @@ import getEnvVars from "../../environment";
 import backend from "../api/backend";
 import { ceil, log } from "react-native-reanimated";
 import Circulos from "../obj/circulos"
-import ScrollH from "../obj/scrollH"
 
 const {apiKey,apiAuthorization,apiImageUrl,apiImageSize} = getEnvVars();
 const {width, height} = Dimensions.get("window");
@@ -35,14 +34,15 @@ const gameInfoScreen = ({route,navigation}) => {
             
         }
     }
+    let screenRoute = "";
     if(pantalla===1){
-        screen = "gameList"
+        screenRoute = "gameList"
     }
     if(pantalla===2){
-        screen = "searchResults"
+        screenRoute = "searchResults"
     }
     if(pantalla===3){
-        screen = "gamesbygenreScreen"
+        screenRoute = "gamesbygenreScreen"
     }
 
     useEffect(()=>{
@@ -62,37 +62,60 @@ const gameInfoScreen = ({route,navigation}) => {
 
 	
     return(
-        <Container style={{backgroundColor:'#0d4b56', flex:1}}>
-            <View style={{flex:2/3, alignItems: 'center', justifyContent: 'center', paddingBottom:-30}}>
-                <ScrollView
-                        horizontal={true}
-                        showsHorizontalScrollIndicator={false}
-                    >
-                        <ScrollH 
-                            ss={game[0].screenshots ? ( {uri:`${apiImageUrl}${apiImageSize}${game[0].screenshots[0].image_id}.jpg`}): require("../../assets/control1.png") }
-                        />
-                        <ScrollH 
-                            ss={game[0].screenshots ? ( {uri:`${apiImageUrl}${apiImageSize}${game[0].screenshots[1].image_id}.jpg`}): require("../../assets/control1.png") }
-                        />
-                </ScrollView>
-            </View>
+        <Container style={{flex:1, backgroundColor:'#ffffd1'}}>
+            
+                <View style={{flex:0.55, alignItems: 'center', justifyContent: 'center',zIndex:-1, position:'absolute'}}>
+                    <ScrollView
+                            horizontal={true}
+                            showsHorizontalScrollIndicator={false}
+                        >
+                            
+                            <View style={{flexDirection:'row', justifyContent:'flex-start',flexWrap:'wrap',alignSelf:'flex-start'}}>
+                                {game[0].screenshots ? game[0].screenshots.map((element,key)=>(
+                                    <Image 
+                                    key={key} 
+                                    style={styles.gameCover1} 
+                                    source={{uri: `${apiImageUrl}${apiImageSize}${element.image_id}.jpg`}} 
+                                    />
+                                )): require("../../assets/control1.png")} 
+                                
+                            </View>
+                    </ScrollView>
+                </View>
 
-            <ScrollView
-                vertical={true}
-                showsVerticalScrollIndicator={false}
-                style={{flex:1/3}}
-            >
-                
-                <Card style={styles.vertical}>
-                    
-                </Card>
-                <Card style={styles.vertical}>
-                    
-                </Card>
-            </ScrollView>
-            <TouchableHighlight onPress={() => navigation.navigate(screen)} style={styles.icono}>
+                <ScrollView
+                    vertical={true}
+                    showsVerticalScrollIndicator={false}
+                    style={{flex:1, marginTop:170}}
+                >
+                    <Card style={styles.vertical}>
+                        <Image source={game[0].cover ? ( {uri:`${apiImageUrl}${apiImageSize}${game[0].cover.image_id}.jpg`}): require("../../assets/control1.png") }
+                                style = {styles.gameCover} />
+                        <H3 style={{color:'#000000',textAlign:'center'}}>{name}</H3>
+                        <CardItem style={{flexDirection:'row', justifyContent:'flex-start',flexWrap:'wrap',alignSelf:'flex-start'}}>
+                            <View >
+                                <Circulos percentage={game[0].rating ? game[0].rating: 0 } color={'#b9da00'} delay={600} max={100}/>
+                            </View>
+                            <View style={{ustifyContent:'flex-start',flexWrap:'wrap',alignSelf:'center', alignContent:'center'}}>
+                                {game[0].platforms.map((element,key)=>(<Text key={key} style={{fontSize:19,color:'#000',textAlign:"center"}}>{element.name}</Text>))}
+                            </View>
+                        </CardItem>
+                        <CardItem>
+                            <Text style={{fontSize:20}}>Rating</Text>
+                            <Text style={{fontSize:19}}>Platforms</Text>
+                        </CardItem>  
+                    </Card>
+                    <Card style={styles.v1}>
+                        <CardItem style={{borderRadius:10}}>
+                            <Text style={{color:'#000000',alignContent:"center",fontSize:20}}>
+                                Summary: {game[0].summary ? game[0].summary:`Description not available!`}
+                            </Text>
+                        </CardItem>
+                    </Card>
+                </ScrollView>
+            <TouchableHighlight onPress={() => navigation.navigate(screenRoute)} style={styles.icono}>
                     <View>
-                        <Icon name="arrow-back" style={{color:"#fff",width:30, zIndex: 1}} />
+                        <Icon name="arrow-back" style={{color:"#000000",width:30}} />
                     </View>
             </TouchableHighlight>
             
@@ -108,14 +131,14 @@ const styles = StyleSheet.create({
         width:width*0.98,
     
     },
-    gameThumbnail:{
-        width:50,
-        height:50,
-        resizeMode:"contain"
+    gameCover1:{
+        width: width,
+        height:height*0.35
     },
     gameCover:{
-        width: width*0.85,
-        height:height*0.5,
+        width: width*0.50,
+        height:height*0.30,
+        resizeMode:"contain"
     },
     ImageNotFound:{
         width : 70,
@@ -126,18 +149,32 @@ const styles = StyleSheet.create({
         position:'absolute',
         marginLeft:10,
         marginTop:10,
+        zIndex:2
     },
     vertical:{
         borderColor:'#fff',
-        backgroundColor:'#ff0000',
+        backgroundColor:'#fff',
         borderWidth:3, 
         borderRadius:10, 
-        height:height*0.5, 
-        width:width*0.9,
+        height:height*0.7, 
         marginLeft:20,
         marginRight:20,
-        marginTop:0,
-        marginBottom:20
+        marginTop:30,
+        marginBottom:10,
+        alignItems:'center',
+    },
+    v1:{
+        borderColor:'#fff',
+        backgroundColor:'#fff',
+        borderWidth:3, 
+        borderRadius:10, 
+        marginLeft:20,
+        marginRight:20,
+        marginTop:30,
+        marginBottom:10,
+        alignItems:'center',
+        justifyContent:'center',
+        flex:1
     },
 });
 
